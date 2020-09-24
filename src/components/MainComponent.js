ef57cf82394e62e97c4c629d726ae7224317350f
korlahwarleh <jmkolawole@gmail.com>
2020-09-24 21:43:40 +0100

    Controlled form validation

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -7,6 +7,7 @@ import Header from './HeaderComponent';
 import Home from "./HomeComponent";
 import Footer from './FooterComponent';
 import Contact from './ContactComponent';
+import About from "./AboutComponent";
 import {DISHES} from '../shared/dishes';
 import {COMMENTS} from '../shared/comments';
 import {LEADERS} from '../shared/leaders';
@@ -52,6 +53,12 @@ class Main extends Component {
 
     }
 
+    const AboutPage = () => {
+        return (
+          <About leaders={this.state.leaders}/>
+        )
+    }
+
 
     return (
       <div className="">
@@ -62,6 +69,7 @@ class Main extends Component {
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>}/>
           <Route path="/menu/:dishId" component={DishWithId}/>
           <Route exact path="/contactus" component={Contact}/>
+          <Route exact path="/aboutus" component={AboutPage}/>
 
           <Redirect to="/home" />
         </Switch>  
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -52,7 +52,5 @@ const Menu = (props) => {
   
 }
 
-
-
-
 export default Menu;
+
